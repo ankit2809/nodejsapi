d6bf25f5471afd477a87bdbdd68deba4377497cb
Ankit <Ankit@m1-4011-a.local>
2020-06-10 19:02:33 +0200

    version 1.3

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express')
+var googleTranslate = require('google-translate')(apiKey, options);
 var app = express()
 port = process.env.PORT || 3000
 app.use(express.static(__dirname))
+
+
+
 app.listen(port);
 console.log('todo list RESTful API server started on: ' + port);
